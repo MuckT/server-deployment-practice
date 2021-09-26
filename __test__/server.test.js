760283731264ef6836c0f3d2ebc8c6c7088dacec
MuckT <mcgtom10@gmail.com>
2021-09-25 18:31:33 -0700

    test: nits for order and style

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -6,26 +6,26 @@ const request = supertest(server.app)
 
 describe('Given GET / ', () => {
   describe('When requested', () => {
-    it('Then response body \'Hello World!\'', async() => {
-      const response = await request.get('/')
-      expect(response.text).toEqual('Hello World!')
-    })
-
-    it('Then returns 200 status', async() => {
+    it('Then returns 200 status', async () => {
       const response = await request.get('/')
       expect(response.status).toEqual(200)
+    })
+
+    it('Then response body \'Hello World!\'', async () => {
+      const response = await request.get('/')
+      expect(response.text).toEqual('Hello World!')
     })
   })
 })
 
 describe('Given GET /iDoNotExist', () => {
   describe('When requested', () => {
-    it('Then returns 404 status', async() => {
+    it('Then returns 404 status', async () => {
       const response = await request.get('/iDoNotExist')
       expect(response.status).toEqual(404)
     })
 
-    it('Then returns error message', async() => {
+    it('Then returns error message', async () => {
       const response = await request.get('/iDoNotExist')
       expect(response.body.message).toEqual('Not Found!')
     })
